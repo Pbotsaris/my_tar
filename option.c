3960cd859d0377691dc311e52bc7f99da4619b0b
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-22 18:30:42 +0300

    Updated option

--- option.c
+++ option.c
@@ -1,4 +1,3 @@
-
 typedef enum
 {
     c,
@@ -6,7 +5,8 @@ typedef enum
     t,
     u,
     x,
-    NONE
+    NONE,
+    ERROR
 } option_t;
 
 typedef enum
@@ -18,39 +18,64 @@ typedef enum
 #define F_NOT_FOUND "my_tar: Refusing to read archive contents from terminal (missing -f option?)\n"
 #define NULL_OPT "my_tar: Error is not recoverable: exiting now\n"
 
-option_t check_option(int argc, char *argv[])
+option_t check_spam(option_t flag)
+{
+    option_t check_flag = NONE;
+
+    if (flag == NONE)
+        return check_flag;
+    else
+        return check_flag = ERROR;
+}
+
+option_t check_option(int argc, char **format)
 {
     int index = 0;
 
     option_t flag_opt = NONE;
-    bool_f flag_bool = FALSE;
+    bool_f flag_f = FALSE;
 
-    while (argv[index] && flag_bool == FALSE)
+    while (format[index] && flag_f == FALSE)
     {
-        if (argv[index] == '-')
+        if (format[index] == '-')
         {
-            if (argv[index][1] == 'c')
-                flag_opt = c;
-            if (argv[index][1] == 't')
-                flag_opt = t;
-            if (argv[index][1] == 'r')
-                flag_opt = r;
-            if (argv[index][1] == 'u')
-                flag_opt = u;
-            if (argv[index][1] == 'x')
-                flag_opt = x;
-            if (argv[index][2] == 'f' || argv[index][1] == 'f')
-                flag_bool = TRUE;
+            switch (format[index][1])
+            {
+            case 'c':
+                if (check_spam(flag_opt) == NONE)
+                    flag_opt = c;
+                break;
+            case 't':
+                if (check_spam(flag_opt) == NONE)
+                    flag_opt = t;
+                break;
+            case 'r':
+                if (check_spam(flag_opt) == NONE)
+                    flag_opt = r;
+                break;
+            case 'u':
+                if (check_spam(flag_opt) == NONE)
+                    flag_opt = u;
+            case 'x':
+                if (check_spam(flag_opt) == NONE)
+                    flag_opt = x;
+                break;
+            case 'f':
+                flag_f = TRUE;
+                break;
+            }
+            if (format[index][2] == 'f' && flag_f != TRUE)
+                flag_f = TRUE;
         }
         index++;
     }
 
-    if (flag_bool == FALSE)
+    if (flag_f == FALSE)
     {
         printf(F_NOT_FOUND);
         flag_opt = NONE;
     }
-    if (flag_opt == NONE)
+    if (flag_opt == NONE || flag_opt == ERROR)
         printf(NULL_OPT);
 
     return flag_opt;
