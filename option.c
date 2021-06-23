9dbc055c4ce8275f3bb68bba5123f595810793c7
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-23 10:06:47 +0300

    option.c

--- option.c
+++ option.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "string_handling.c"
+
 typedef enum
 {
     c,
@@ -6,16 +9,18 @@ typedef enum
     u,
     x,
     NONE,
-    ERROR
+    ERROROPT
 } option_t;
 
 typedef enum
 {
     TRUE,
-    FALSE
+    FALSE,
+    ERRORF
 } bool_f;
 
 #define F_NOT_FOUND "my_tar: Refusing to read archive contents from terminal (missing -f option?)\n"
+#define F_ERROR "You must specify one of the the following options -c -r -t -u -x"
 #define NULL_OPT "my_tar: Error is not recoverable: exiting now\n"
 
 option_t check_spam(option_t flag)
@@ -25,10 +30,55 @@ option_t check_spam(option_t flag)
     if (flag == NONE)
         return check_flag;
     else
-        return check_flag = ERROR;
+        return check_flag = ERROROPT;
 }
 
-option_t check_option(int argc, char **format)
+size_t my_strlen(char *str)
+{
+    int counter = 0;
+
+    while (str[counter])
+        counter++;
+
+    return counter;
+}
+
+bool_f check_f(bool_f flag_f, option_t flag_opt, char **format, int index)
+{
+    if (flag_opt == NONE)
+        if (format[index][0] == 'f')
+            return ERRORF;
+
+    if (flag_opt != NONE && format[index][0] == 'f')
+        return TRUE;
+
+    if (flag_f == FALSE && format[index][1] == 'f')
+        return TRUE;
+
+    if (flag_f == TRUE)
+        return ERRORF;
+}
+
+option_t error_handler(bool_f flag_f, option_t flag_opt)
+{
+    if (flag_f == FALSE)
+    {
+        printf(F_NOT_FOUND);
+        flag_opt = ERROROPT;
+    }
+    if (flag_opt == NONE || flag_opt == ERROROPT)
+        printf(NULL_OPT);
+
+    if (flag_f == ERRORF)
+    {
+        printf(F_ERROR);
+        flag_opt = ERROROPT;
+    }
+
+    return flag_opt;
+}
+
+option_t check_option(char **format)
 {
     int index = 1;
 
@@ -39,46 +89,45 @@ option_t check_option(int argc, char **format)
     {
         if (format[index][0] == '-')
         {
-            switch (format[index][1])
-            {
-            case 'c':
-                if ((flag_opt = check_spam(flag_opt)) == NONE)
-                    flag_opt = c;
-                break;
-            case 't':
-                if ((flag_opt = check_spam(flag_opt)) == NONE)
-                    flag_opt = t;
-                break;
-            case 'r':
-                if ((flag_opt = check_spam(flag_opt)) == NONE)
-                    flag_opt = r;
-                break;
-            case 'u':
-                if ((flag_opt = check_spam(flag_opt)) == NONE)
-                    flag_opt = u;
-            case 'x':
-                if ((flag_opt = check_spam(flag_opt)) == NONE)
-                    flag_opt = x;
-                break;
-            case 'f':
-                flag_f = TRUE;
-                break;
-            }
+            if ((flag_opt = check_spam(flag_opt)) == NONE)
+                if (my_strlen(format[index]) == 1 || (my_strlen(format[index]) == 2 && format[index][1] == 'f'))
+                {
+                    switch (format[index][1])
+                    {
+                    case 'c':
+                        flag_opt = c;
+                        break;
+                    case 't':
+                        flag_opt = t;
+                        break;
+                    case 'r':
+                        flag_opt = r;
+                        break;
+                    case 'u':
+                        flag_opt = u;
+                    case 'x':
+                        flag_opt = x;
+                        break;
+                    }
+                }
+                else
+                    flag_opt = ERROROPT;
         }
 
-        if (format[index][2] == 'f' && flag_f != TRUE)
-            flag_f = TRUE;
+        flag_f = check_f(flag_f, flag_opt, format, index);
 
         index++;
     }
 
-    if (flag_f == FALSE)
-    {
-        printf(F_NOT_FOUND);
-        flag_opt = NONE;
-    }
-    if (flag_opt == NONE || flag_opt == ERROR)
-        printf(NULL_OPT);
+    flag_opt = error_handler(flag_f, flag_opt);
 
     return flag_opt;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc > 2)
+        return 0;
+    check_option(argv);
+    return 0;
 }
