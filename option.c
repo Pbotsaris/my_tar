6fb0db7ff1250200a55a36c9616c67fbcee6a99a
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-23 10:38:41 +0300

    options

--- option.c
+++ option.c
@@ -113,8 +113,8 @@ option_t check_option(char **format)
     return flag_opt;
 }
 
-int main(int argc, char **format)
-{
-    check_option(format);
-    return 0;
-}
+// int main(int argc, char **format)
+// {
+//     check_option(format);
+//     return 0;
+// }
