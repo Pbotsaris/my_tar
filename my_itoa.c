664d35cf2315f49e9bb5dda540ebac2577e89ac2
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-23 17:29:56 +0300

    itoa

--- my_itoa.c
+++ my_itoa.c
@@ -1,38 +1,39 @@
-#include "my_itoa.h"
-#include <stdio.h>
+#include "my_tar.h"
 
 void reverse(char str[], int length)
 {
-     char temp_str[length];
-    for(int i = 0; i < length; ++i)
+    char temp_str[length];
+    for (int i = 0; i < length; ++i)
         temp_str[i] = str[i];
 
     int end = length - 1;
-    for(int i = 0; i < length; ++i){
-       str[i] = temp_str[end];
-         end--;
+    for (int i = 0; i < length; ++i)
+    {
+        str[i] = temp_str[end];
+        end--;
     }
 }
 
-void my_itoa(char* str, int num, int base)
+void my_itoa(char *str, int num, int base)
 {
-		printf("number going in:  %o\n", num);
+    printf("number going in:  %o\n", num);
     int idx = 0;
-    if (num == 0){
+    if (num == 0)
+    {
         str[idx] = '0';
-        return ;
-    
-    // negative with base 10 only
+        return;
+
+        // negative with base 10 only
     }
 
-    while (num != 0){
+    while (num != 0)
+    {
         int remainder = num % base;
         // remainder returns leftmost digits - 9 % 10 = 9  19 % 10 = 9
         str[idx++] = (remainder > 9) ? (char)((remainder - 10) + 'a') : (char)(remainder + '0');
-        num = num/base;
+        num = num / base;
     }
 
     reverse(str, idx);
     str[idx] = '\0';
 }
-
--- my_itoa.h
+++
@@ -1,3 +0,0 @@
-// typedef enum {TRUE, FALSE}bool_t;
-
-void my_itoa(char *str, int num, int base);
