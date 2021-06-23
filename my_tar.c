843b2eb9730181a0de628a769a17964c1b6c5a98
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-23 11:20:40 +0300

    inital main

---
+++ my_tar.c
@@ -0,0 +1,25 @@
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_tar.c
+ *
+ *    Description:  A naive implementation with the infamous tar
+ *
+ *        Version:  0.1
+ *        Created:  06/22/2021 16:05:36
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Khalil Masri, Pedro Botsaris
+ *   Organization:  ELU
+ *
+ * =====================================================================================
+ */
+#include "option.c"
+
+int main(int argc, char *argv[])
+{
+    check_option(argv);
+    return 0;
+}
