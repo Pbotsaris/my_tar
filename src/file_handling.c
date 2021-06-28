4e1616becb76488fa8a8779950074840def68cb9
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-28 11:35:03 +0300

    fixed checksum

--- src/file_handling.c
+++ src/file_handling.c
@@ -13,7 +13,6 @@
 /*blksize_t st_blksize;     [> Block size for filesystem I/O <]*/
 /*blkcnt_t  st_blocks;      [> Number of 512B blocks allocated <]*/
 
-
 // S_ISDIR(stats.st_mode);
 
 unsigned int checksum_calculator(char *header, size_t size)
@@ -49,7 +48,6 @@ void checksum(header_t *header)
 	sprintf(header->chksum, "%0*o ", 8, check);
 }
 
-
 void file_info(header_t *header, struct stat stats)
 {
 
@@ -74,19 +72,18 @@ void file_info(header_t *header, struct stat stats)
 
 void add_uname_gname(header_t *header, struct stat stats)
 {
-  struct passwd *pws;
+	struct passwd *pws;
 	struct group *grp;
-  pws = getpwuid(stats.st_uid);
+	pws = getpwuid(stats.st_uid);
 	grp = getgrgid(stats.st_gid);
 
 	strcpy(header->gname, grp->gr_name);
 	strcpy(header->uname, pws->pw_name);
 }
 
-
 void add_mode(header_t *header, struct stat stats)
 {
-// ->  TODO:	The mode field provides nine bits specifying file permissions and three bits to specify the Set UID, Set GID, and Save Text (sticky) modes.
+	// ->  TODO:	The mode field provides nine bits specifying file permissions and three bits to specify the Set UID, Set GID, and Save Text (sticky) modes.
 	int mode = 0;
 
 	int modes[NUM_MODES] = {TUREAD, TUWRITE, TUEXEC, TGREAD, TGWRITE, TGEXEC, TOREAD, TOWRITE, TOEXEC};
@@ -99,28 +96,28 @@ void add_mode(header_t *header, struct stat stats)
 	my_itoa(header->mode, mode, OCTAL);
 }
 
-
-		/* TODO: double check -
+/* TODO: double check -
 		 * The name field is the file name of the file, with directory names (if any) preceding the file name, separated by slashes. */
 void add_name(header_t *header, char *path)
 {
-   /* name is prefix if more than 100 char */
-  size_t path_len = strlen(path);
-  if(path_len < MAX_NAME_SIZE){
+	/* name is prefix if more than 100 char */
+	size_t path_len = strlen(path);
+	if (path_len < MAX_NAME_SIZE)
+	{
 		strcpy(header->name, path);
 		header->prefix[0] = '\0';
 	}
 
-	else if(path_len < MAX_NAME_SIZE * 2){ 
+	else if (path_len < MAX_NAME_SIZE * 2)
+	{
 		strncpy(header->prefix, path, MAX_NAME_SIZE);
 		header->prefix[MAX_NAME_SIZE - 1] = '\0';
 		strncpy(header->name, &path[MAX_NAME_SIZE - 1], MAX_NAME_SIZE);
 		header->name[MAX_NAME_SIZE - 1] = '\0';
-		}
+	}
 
-	 else
-		 printf("%s", EXC_NAME_SIZE);
-
+	else
+		printf("%s", EXC_NAME_SIZE);
 }
 
 header_t *create_header(char *path)
@@ -132,7 +129,7 @@ header_t *create_header(char *path)
 	if (stat(path, &stats) == 0)
 	{
 
-    add_name(header, path);
+		add_name(header, path);
 		add_mode(header, stats);
 		add_uname_gname(header, stats);
 		file_info(header, stats);
