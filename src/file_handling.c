65f78a99a3be07c1fc490661c4f2d7a88a105945
Pedro Botsaris <pedro@antfood.com>
2021-06-28 13:25:26 +0200

    added check for symlink

--- src/file_handling.c
+++ src/file_handling.c
@@ -13,6 +13,16 @@ unsigned int checksum_calculator(char *header, size_t size)
 	return check;
 }
 
+
+bool_t symlink_exists(const char* path)
+{
+    struct stat stats;
+    int result;
+    result = lstat(path, &stats);
+    return (result == 0);
+}
+
+
 void checksum(header_t *header)
 {
 	unsigned int check = 0;
@@ -52,24 +62,28 @@ void file_info(header_t *header, struct stat stats)
 //	my_itoa(header->mtime, stats.st_mtim.tv_sec, DECIMAL);
 
 	/* [> Check Sum <] */
-	checksum(header);
+	//checksum(header);
 }
 
 
-void add_dev_major_minor(header_t *header, struct  stat stats)
+void add_dev_major_minor(header_t *header, struct stat stats)
 {
 			  my_itoa(header->devmajor, (int)major(stats.st_rdev), DECIMAL);
 			  my_itoa(header->devminor, (int)minor(stats.st_rdev), DECIMAL);
 }
 
 
-void add_filetype(header_t *header, struct stat stats)
+void add_filetype(header_t *header, struct stat stats, char * path)
 {
 
 	if(S_ISDIR(stats.st_mode))
 		 header->typeflag = DIRTYPE; 
-	else if(S_ISREG(stats.st_mode))
-		header->typeflag = REGTYPE;
+	else if(S_ISREG(stats.st_mode)){
+		if(symlink_exists(path) == TRUE)
+			header->typeflag =  SYMTYPE;
+		else
+			header->typeflag = REGTYPE;
+	}
 	else if(S_ISCHR(stats.st_mode)){
 		header->typeflag =CHRTYPE;
 		add_dev_major_minor(header, stats);
@@ -80,12 +94,13 @@ void add_filetype(header_t *header, struct stat stats)
 	}
 	else if(S_ISFIFO(stats.st_mode))
 		header->typeflag = FIFOTYPE;
-	else if(S_ISLNK(stats.st_mode))
-		header->typeflag = SYMTYPE;
+	else if(S_ISLNK(stats.st_mode)){
+		header->typeflag =  LNKTYPE;
+	}
 	// if not none above
 	else{
-		header->typeflag = REGTYPE;
-		printf("%s\n", FLAGTYPE_ERR);
+					header->typeflag = REGTYPE;
+			printf("%s\n", FLAGTYPE_ERR);
 	}
 		
 }
@@ -105,7 +120,6 @@ void add_mode(header_t *header, struct stat stats)
 {
 	// ->  TODO:	The mode field provides nine bits specifying file permissions and three bits to specify the Set UID, Set GID, and Save Text (sticky) modes.
 	int mode = 0;
-
 	int modes[NUM_MODES] = {TUREAD, TUWRITE, TUEXEC, TGREAD, TGWRITE, TGEXEC, TOREAD, TOWRITE, TOEXEC};
 	int stats_modes[NUM_MODES] = {S_IREAD, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
 
@@ -145,13 +159,11 @@ header_t *create_header(char *path)
 	struct stat stats;
 	if (stat(path, &stats) == 0)
 	{
-
 		add_name(header, path);
 		add_mode(header, stats);
-		add_filetype(header, stats);
+		add_filetype(header, stats, path);
 		add_uname_gname(header, stats);
 		file_info(header, stats);
-		
 	}
 	else
 	{
