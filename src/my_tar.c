0c509cfbc074b8e9bcc7dc6bed8f9700235e1cb2
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-06-28 21:24:37 +0300

    commented things in main

--- src/my_tar.c
+++ src/my_tar.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
     // check_option(argv);
 
-    if(argc < 2)
+    if (argc < 2)
         return 0;
 
     header_t *header;
@@ -39,17 +39,17 @@ int main(int argc, char *argv[])
         header = create_header(argv[i]);
     }
 
+    archive(argv[1], argv, argc);
 
-    printf("name: %s\n prefix: %s\n", header->name, header->prefix);
-    printf("type: %c\n", header->typeflag);
-    printf("dev minor: %s , dev major: %s\n", header->devminor, header->devmajor);
-    printf("mode in char(octal): %s\nUSER ID: %s\nGROUP OWNER ID: %s\nSize: %s\n", header->mode, header->uid, header->gid, header->size);
-    printf("Modified time in seconds: %s\n", header->mtime);
-    printf("Link name: %s\n", header->linkname);
-    printf("CHKSUM: %s\n", header->chksum); 
-    printf("group name: %s\n user name: %s\n", header->gname, header->uname);
-    printf("mode in char(octal): %s\nUSER ID: %s\nGROUP OWNER ID: %s\nSize: %s\nLink: %s\n", header->mode, header->uid, header->gid, header->size, header->linkname);
-
+    // printf("name: %s\n prefix: %s\n", header->name, header->prefix);
+    // printf("type: %c\n", header->typeflag);
+    // printf("dev minor: %s , dev major: %s\n", header->devminor, header->devmajor);
+    // printf("mode in char(octal): %s\nUSER ID: %s\nGROUP OWNER ID: %s\nSize: %s\n", header->mode, header->uid, header->gid, header->size);
+    // printf("Modified time in seconds: %s\n", header->mtime);
+    // printf("Link name: %s\n", header->linkname);
+    // printf("CHKSUM: %s\n", header->chksum);
+    // printf("group name: %s\n user name: %s\n", header->gname, header->uname);
+    // ls_tar(argv[1]);
     free(header);
 
     return 0;
