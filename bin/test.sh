30a011a76faf27a5d87b0626f1870ab1059a23cf
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-07-06 14:30:46 +0300

    modified for flag -cf

--- bin/test.sh
+++ bin/test.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-rm *.tar
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  gtar -cf real.tar test_files/f.txt
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  tar -cf real.tar test_files/f.txt
-fi
-./bin/my_tar -cf $1 fake.tar test_files/f.txt
-echo "real"
-cat real.tar
-echo "fake"
-cat fake.tar
->>>>>>> 9c69f62e56e6ccffd16c4effe5533e806b1ed1fb
-
-=======
->>>>>>> 9a66417fa5a59ec156dada4c2c52e6fa32fce87a
 run_tar(){
   if [[ "$OSTYPE" == "darwin"* ]]; then
     gtar -cf real.tar $1
