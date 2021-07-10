4f92453480c81346e0e682cfb1f19141fc222bc3
Pedro Botsaris <pedro@antfood.com>
2021-07-10 18:28:51 +0200

    extract multiple regular files

--- src/list_and_extract.c
+++ src/list_and_extract.c
@@ -17,10 +17,11 @@ void make_directory(char *path)
 {
     struct stat stats;   
     if (stat (path, &stats) == 0)
-        printf("The directory %s already exists. Unable to extract." , path);
+        printf("The directory %s already exists. Unable to extract. \n" , path);
     else
         mkdir(path, S_IWUSR | S_IXUSR | S_IRUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH); 
 }
+
 
 int touch(int tar, header_t *header)
 {
@@ -28,22 +29,17 @@ int touch(int tar, header_t *header)
     {
         int dest = open(header->name, O_CREAT | O_WRONLY);
         if (dest < 0){
-            printf("Could not create file %s for extraction.", header->name);
+            printf("Could not create file %s for extraction.\n", header->name);
             return -1;
         }
-
-        int num_of_blocks = decimal_to_octal(BLOCKSIZE)/ atoi(header->size);
-        if(num_of_blocks >= 1)
-            write_file(dest, tar);
-        else {
-            for (int i = 0; (num_of_blocks - i) >= 1; i++)
-                write_file(dest, tar);
-
-            close(dest);
-        }
+        write_file(dest, tar);
+        chmod(header->name, atoi(header->mode));
     }
+
+    printf("The file %s was succesfully extracted. \n", header->name);
     return 0;
 }
+
 
 void extract(int tar, int file_position, int end_file)
 {
@@ -51,19 +47,17 @@ void extract(int tar, int file_position, int end_file)
     {
         header_t *header = malloc(sizeof(header_t));
         header = get_header(tar);
+        lseek(tar, ENDBLK, SEEK_CUR);
 
-        if (header->name[0] != '\0')
-            printf("%s\n", header->name);
         if (header->typeflag != DIRTYPE){
             touch(tar, header);
-       //     file_position = lseek(tar, next_file_position(header), SEEK_CUR);
+            file_position = lseek(tar, next_header_position(header) - BLOCKSIZE, SEEK_CUR);
         }
         else
         {
             make_directory(header->name);
             file_position += BLOCKSIZE;
         }
-
         free(header);
     }
 
@@ -71,7 +65,6 @@ void extract(int tar, int file_position, int end_file)
 
 void list(int tar, int file_position, int end_file)
 {
-
     while (file_position <= end_file)
     {
         header_t *header = malloc(sizeof(header_t));
