f414db6d83bdb3c1d327fadccbfce5312041934b
khalilmasri <33801628+khalilmasri@users.noreply.github.com>
2021-07-12 18:36:07 +0300

    Successful extracting prints only if there is a header_name

--- src/list_and_extract.c
+++ src/list_and_extract.c
@@ -40,8 +40,9 @@ int touch(int tar, header_t *header)
         chmod(header->name, octal_to_decimal(atoi(header->mode)));
     }
 
+    if(header->name[0] != '\0')
+        printf("The file %s was succesfully extracted. \n", header->name);
 
-    printf("The file %s was succesfully extracted. \n", header->name);
     return 0;
 }
 
